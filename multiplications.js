5260879cbac31af0323b6d7b995b8214d9b64a53
MTRG15 <mtrg1500@gmail.com>
2020-07-13 08:24:33 -0400

    Adding configuration options
    Multiplying tables are now selectable to study a specific one.
    Added a non-functional statistic function

--- multiplications.js
+++ multiplications.js
@@ -1,45 +1,108 @@
-var m1 = document.getElementById("mult1")
-var m2 = document.getElementById("mult2")
-var r = document.getElementById("respuesta")
+
+// variables refresh()
+var mult = new Array( document.getElementById("mult1"),  document.getElementById("mult2") )
+var respuesta = document.getElementById("respuesta")
 var buenas = 0
 var malas = 0
 var num1
 var num2
+var mensaje1 = document.getElementById("mensaje1")
+var mensaje2 = document.getElementById("mensaje2")
 
-newoperation()
+// variables checkConf()
+var boxes = new Array( 
+	document.getElementById("tabla1"),
+	document.getElementById("tabla2"),
+	document.getElementById("tabla3"),
+	document.getElementById("tabla4"),
+	document.getElementById("tabla5"),
+	document.getElementById("tabla6"),
+	document.getElementById("tabla7"),
+	document.getElementById("tabla8"),
+	document.getElementById("tabla9"),
+	document.getElementById("tabla10"),
+	document.getElementById("tabla11"),
+	document.getElementById("tabla12")
+	)
+var multPool = new Array() //stores the possible numbers to multiply by
+var mensaje3 = document.getElementById("mensaje2")
 
-window.onkeypress = function(event) {
+// variables statistics()
+var prevQuestions = new Array(2,3) //stores all asked questions
+var timer = new Array(1)
+
+clearCheckBoxes() // clears the configuration to default
+newOperation() // Creando la primera operacion al abrir la pagina
+
+window.onkeypress = function(event) { //detecta la techa enter para revisar la respuesta
    if (event.keyCode == 10 || event.keyCode == 13) {
-   	console.log("ha presionado enter")
       refresh()
-   }else{
-   	console.log(event.keyCode)
    }
 }
 
-function refresh(){
-	num1 = m1.innerHTML
-	num2 = m2.innerHTML
-	var larespuesta = r.value
-	var trueanswer = num1 * num2
-	if(larespuesta == trueanswer){
-		document.getElementById("mensaje").innerHTML = "¡Bien!"
-		buenas = buenas + 1
+function refresh(){ // verifica la respuesta y crea una nueva operacion
+	num1 = mult[0].innerHTML
+	num2 = mult[1].innerHTML
+	var givenAnswer = respuesta.value
+	var trueAnswer = num1 * num2
+	if(givenAnswer == trueAnswer){
+		mensaje1.innerHTML = "¡Bien!"
+		statistics(true)
 	}else{
-		document.getElementById("mensaje").innerHTML ="No, la respuesta es " + num1 + " x " + num2 + " = " + trueanswer + ", ¡Sigue Intentando!"
-		malas = malas + 1
+		mensaje1.innerHTML ="No, la respuesta es " + num1 + " x " + num2 + " = " + trueAnswer + "  ¡Sigue intentando!"
+		statistics(false)
 	}
 
 	if(buenas >= 20){
-		document.getElementById("mensaje2").innerHTML = "Excelente, puedes tomar un descanso"
+		mensaje2.innerHTML = "Buen trabajo, puedes tomar un descanso"
 	}
-
-	newoperation()
+	
+	newOperation()
+	
 }
 
-function newoperation(){
-	m1.innerHTML = Math.floor((Math.random() * 5)+2)
-	m2.innerHTML = Math.floor((Math.random() * 11)+2)
-	r.value = ""
-	r.focus()
+function newOperation(){ // creates a new operation to solve
+	if( multPool.length >= 1 ){ 
+		mult[0].innerHTML = multPool[Math.floor((Math.random() * multPool.length))] }
+	else{ 
+		mult[0].innerHTML = Math.floor((Math.random() * 12) + 1) }
+	mult[1].innerHTML = Math.floor((Math.random() * 12)+1)
+	
+	respuesta.value = ""
+	respuesta.focus()
+}
+
+function timerTick( value, index ){
+	timer[index] = timer[index] - 1
+}
+
+function clearCheckBoxes(){
+	boxes.forEach( function (value, index){
+		boxes[index].checked = false
+	})
+}
+
+function checkConfig(){ //checks the checkboxes to update the operation pool
+	multPool.length = 0 // clearing the array
+	for (var i = boxes.length - 1; i >= 0; i--) {
+		if( boxes[i].checked ){
+			multPool.push(i+1)
+		}
+	}
+	if( multPool.length == 0 ){
+		mensaje2.innerHTML = "No seleccionaste ninguna tabla, ¡Repasemos todas!"
+	}else{
+		mensaje2.innerHTML = "Seleccionaste las tablas del " + multPool.toString()
+	}
+	newOperation()
+}
+
+function statistics( answer ){ //true = correct answer, false = incorrect answer
+	if(answer == false){ // a bad answer is saved to be asked again shortly after
+		prevQuestions.push([num1, num2])
+		timer.push(3)
+		malas = malas + 1
+	}else if( num1 > 1 ){
+		buenas = buenas + 1
+	}
 }
