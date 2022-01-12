fb384a309db81781794fc47a6bfd257e0ce8b072
MTRG15 <mtrg1500@gmail.com>
2022-01-12 14:02:59 -0400

    Opening of IdeaLang first assignment

---
+++ IdeaLang/bootstrap-test/custom-js/button-behavior.js
@@ -0,0 +1,13 @@
+// Button response on click
+function correct(button){
+    button.classList.remove("btn-primary");
+    button.classList.add("btn-success");
+    button.innerText = "Correct!";
+}
+
+function incorrect(button){
+    button.classList.remove("btn-primary");
+    button.classList.add("btn-danger");
+    button.innerText = "Incorrect!";
+}
+
